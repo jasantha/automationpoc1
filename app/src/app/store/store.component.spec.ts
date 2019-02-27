9b43bf59ab3cb9f3d7110dd6ce69d349ec1fcf59
sreehaas <sreehaas@gmail.com>
2019-02-26 23:02:42 -0500

    changes to both stores and products

--- app/src/app/store/store.component.spec.ts
+++ app/src/app/store/store.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StoreComponent } from './store.component';
 //UI_TESTS_TAGS:stores
-//test
+//demo
 describe('StoreComponent', () => {
   let component: StoreComponent;
   let fixture: ComponentFixture<StoreComponent>;
