714e09cb33c183d694429c53de0de62870043941
sreehaas <sreehaas@gmail.com>
2019-02-27 01:00:05 -0500

    master

--- app/src/app/products/products.component.spec.ts
+++ app/src/app/products/products.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
+//UI_TESTS_TAGS:products
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
